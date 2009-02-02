7082bf86bbfd6ead41fbcccbe850c91e93e1eff6
Markus Dittrich <markus.lists@gmail.com>
2009-02-01 23:03:58 -0500

    Added a complete implementation for a calculator.
    So far, I am only playing with the parser and all that.

--- src/husky.hs
+++ src/husky.hs
@@ -21,37 +21,103 @@
 -- | main archy driver
 module Main where
 
+-- imports
+import Text.ParserCombinators.Parsec 
+import qualified Text.ParserCombinators.Parsec.Token as PT
+import Text.ParserCombinators.Parsec.Language (emptyDef)
 
--- global imports
-import Data.List
-
--- local imports
-import ApplicativeParsec
 
 -- | main
 main :: IO ()
 main = do
 
-  -- shell prompt
-  putStrLn "husky <> "
-
   -- get a line from stdin
   input <- getLine
 
   -- parse it
-  case parse parse_cl "" input of
-    Left _   -> putStrLn "Error: Unknown action"
-    Right cl -> mapM_ (putStr) (intersperse ":" cl)
+  case parse parse_calc "" input of
+    Left er  -> putStrLn $ "Error: " ++ (show er)
+    Right cl -> putStrLn (show cl)
 
   main
 
 
---data ClString = ClString String | ClStrings [String]
 
--- | simple command line parser
-parse_cl :: CharParser () [String]
-parse_cl = spaces *> parse_strings
+-- | function generating a token parser based on a 
+-- lexical parsers combined with a language record definition
+lexer :: PT.TokenParser st
+lexer  = PT.makeTokenParser emptyDef 
 
-  where
-    parse_strings :: CharParser () [String]
-    parse_strings = (many1 letter) `sepBy` spaces  
+
+-- | token parser for parenthesis
+parens :: CharParser st a -> CharParser st a
+parens = PT.parens lexer
+
+
+-- | token parser for Integer
+integer :: CharParser st Integer
+integer = PT.integer lexer
+
+
+-- | token parser for Double
+float :: CharParser st Double
+float = PT.float lexer
+
+
+-- | token parser for Either Integer Double
+naturalOrFloat :: CharParser st (Either Integer Double)
+naturalOrFloat = PT.naturalOrFloat lexer
+
+
+-- | token parser for keywords
+reservedOp :: String -> CharParser st ()
+reservedOp = PT.reservedOp lexer
+
+-- | token parser for operators
+operators :: String -> CharParser st String
+operators = PT.operator lexer
+
+
+-- | helper function for defining real powers
+real_exp :: Double -> Double -> Double
+real_exp a x = exp $ x * log a
+
+
+
+-- | grammar description for parser
+parse_calc :: CharParser () Double  
+parse_calc = mul_term `chainl1` add_action
+
+mul_term :: CharParser () Double
+mul_term = exp_term `chainl1` multiply_action
+
+exp_term :: CharParser () Double
+exp_term = factor `chainl1` exp_action
+
+factor :: CharParser () Double
+factor = parens parse_calc 
+         <|> parse_sqr
+         <|> parse_number
+
+parse_sqr :: CharParser () Double
+parse_sqr = reservedOp "sqrt" >> parens parse_calc >>= 
+            \x -> return $ sqrt x
+
+multiply_action :: CharParser () (Double -> Double -> Double)
+multiply_action = do {operator "*"; return (*) }
+                  <|> do {operator "/"; return (/) }
+
+add_action :: CharParser () (Double -> Double -> Double)
+add_action = do { operator "+"; return (+) }
+             <|> do { operator "-"; return (-) }
+
+exp_action :: CharParser () (Double -> Double -> Double)
+exp_action = operator "^" >> return real_exp
+
+parse_number :: CharParser () Double
+parse_number = naturalOrFloat >>= 
+               \num -> case num of 
+                 Left i  -> return $ fromInteger i
+                 Right x -> return x
+
+
